fb1b4318039c7e0d1f661d50a349af07fd03cb46
Shatsckij <shatsckij.i@yandex.ru>
2023-08-26 00:31:32 +0300

    init

---
+++ src/main/java/ru/vpt/cocv1/CocV1Application.java
@@ -0,0 +1,13 @@
+package ru.vpt.cocv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CocV1Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/vpt/cocv1/CocV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vpt.cocv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
